b02b7fee21af960a014acbc1616e0f79c32b3476
davi <daviforever17@gmail>
2024-11-23 14:37:27 -0300

    chamados.

--- src/pages/listar/index.jsx
+++ src/pages/listar/index.jsx
@@ -9,7 +9,37 @@ export default function Home() {
         
             <Cabecalho />
 
-        
+            <div className='input'>
+                <input type="text" placeholder='Filtrar...'/>
+                <button></button>
+            </div>
+
+            <table>
+
+                <thead>
+                    <tr>
+                        <th className='title-list'>Título</th>
+                        <th className='a'>Impacto</th>
+                        <th className='a'>Data Ocorrência</th>
+                        <th className='atributed-actions'>Atribuído</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+
+                    <td>JWt Errordfftyr6t</td>
+                    <td className='center'>Error 2</td>
+                    <td className='center'>Error 3</td>
+                    <td>Error 4</td>
+                </tbody>
+                <tbody>
+                    <td>JWt Errordfftyr6t</td>
+                    <td className='center'>Error 2</td>
+                    <td className='center'>Error 3</td>
+                    <td>Error 4</td>
+                </tbody>
+
+            </table>
 
         </div>
 
